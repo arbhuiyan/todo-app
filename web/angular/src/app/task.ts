9b01eba5f9c55723decb369686a65b8c45f54763
Abdur <abdurbhuiya@gmail.com>
2019-06-21 21:16:32 +0100

    add priorities definition

--- web/angular/src/app/task.ts
+++ web/angular/src/app/task.ts
@@ -5,6 +5,10 @@ export interface Task {
  priority: number,
  state?: string   
 }
-
-export const TODO_STATE = "todo";
-export const COMPLETED_STATE = "completed";
+export interface Priority {
+    name: string,
+    value: number
+}
+export const TODO_STATE: string = "todo";
+export const COMPLETED_STATE: string = "completed";
+export const PRIORITIES: Priority[] = [{name:"Normal", value: 1}, {name:"Medium", value: 2}, {name:"High", value: 3}];
