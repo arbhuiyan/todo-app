9290a7f224f56a6604ec4e22f768495be55fc2e9
Abdur <abdurbhuiya@gmail.com>
2019-06-23 01:57:21 +0100

    fix env var

--- web/angular/src/environments/environment.ts
+++ web/angular/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   taskApiUrl: "api",
   // taskApiUrl: "http://192.168.56.102:3000",
   taskIdentifier: "id",
